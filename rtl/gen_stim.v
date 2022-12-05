e0c1aae5638bbd2821497770ad5ccd5f3f40cc34
Spencer Chang <schang412@gmail.com>
2022-12-04 23:04:25 -0800

    rtl

--- rtl/gen_stim.v
+++ rtl/gen_stim.v
@@ -1,28 +1,34 @@
 module gen_stim #(
-    parameter CLK_US = 2,
-    parameter STIM_WIDTH_US = 1000,
-    parameter STIM_GAP_US = 1000,
-    parameter STIM_PERIOD_US = 600000
+    parameter CLK_PERIOD_US = 2
 )
 (
     input  wire CLK_500K,
 
     output wire VSTIM_P,
     output wire VSTIM_N,
+    output wire DO_SAMPLE,
 
+    // this divides the clock by a prescalar to produce the clock for do_sample
     input  wire [3:0] MEASURE_PRESCALAR,
-
-    output wire DO_SAMPLE
+    
+    // this times the clock period will give you the pulse width of a positive or negative stimulus
+    input  wire [9:0] STIM_WIDTH_CYCLES,  
+    
+    // this times the clock period will give you the gap between the stimulus pulse (physiological effects) and the charge recovery pulse (the pulse used to reverse the direction of electrochemical processes incurred by the stimulus)
+    input  wire [9:0] STIM_GAP_CYCLES,  
+    
+    // this times the clock period will give you the period between stimulus events
+    input  wire [19:0] STIM_PERIOD_CYCLES
 );
-localparam PLUS_STIM_END = STIM_WIDTH_US/CLK_US;
-localparam NEG_STIM_BEGIN = PLUS_STIM_END + STIM_GAP_US/CLK_US;
-localparam NEG_STIM_END = NEG_STIM_BEGIN + STIM_WIDTH_US/CLK_US;
 
 reg VSTIM_P_Q = 0;
 reg VSTIM_N_Q = 0;
 reg DO_SAMPLE_Q = 0;
 
 reg [3:0] MEASURE_PRESCALAR_Q = 0;
+reg [9:0] STIM_WIDTH_CYCLES_Q = 500;
+reg [9:0] STIM_GAP_CYCLES_Q = 500;
+reg [19:0] STIM_PERIOD_CYCLES_Q = 300000;
 reg [31:0] counter = 0;
 
 assign VSTIM_P = VSTIM_P_Q;
@@ -34,12 +40,15 @@ always @(posedge CLK_500K) begin
     VSTIM_N_Q <= 0;
     counter <= counter + 1;
     MEASURE_PRESCALAR_Q <= MEASURE_PRESCALAR;
+    STIM_WIDTH_CYCLES_Q <= STIM_WIDTH_CYCLES;
+    STIM_GAP_CYCLES_Q <= STIM_GAP_CYCLES;
+    STIM_PERIOD_CYCLES_Q <= STIM_PERIOD_CYCLES;
 
-    if (counter <= NEG_STIM_END) begin
+    if (counter <= 2*STIM_WIDTH_CYCLES_Q+STIM_GAP_CYCLES_Q) begin
         DO_SAMPLE_Q <= 0;
-        if (counter <= PLUS_STIM_END)
+        if (counter <= STIM_WIDTH_CYCLES_Q)
             VSTIM_P_Q <= 1;
-        else if (counter >= NEG_STIM_BEGIN)
+        else if (counter >= STIM_WIDTH_CYCLES_Q+STIM_GAP_CYCLES_Q)
             VSTIM_N_Q <= 1;
     end else begin
         if (counter % MEASURE_PRESCALAR_Q == 0) begin
@@ -47,9 +56,14 @@ always @(posedge CLK_500K) begin
         end
     end
 
-    if (counter >= STIM_PERIOD_US/CLK_US) begin
+    if (counter >= STIM_PERIOD_CYCLES_Q) begin
         counter <= 0;
     end
+end
 
+initial begin
+    $display("Default Stimulation Pulse Width:                   %d us", STIM_WIDTH_CYCLES_Q*CLK_PERIOD_US);
+    $display("Default Gap between Stimulus and Recovery Pulse:   %d us", STIM_GAP_CYCLES_Q*CLK_PERIOD_US);
+    $display("Default Period between Stimulus Pulses:            %d us", STIM_GAP_CYCLES_Q*CLK_PERIOD_US);
 end
 endmodule
--- rtl/tb/test_gen_stim.py
+++ rtl/tb/test_gen_stim.py
@@ -10,11 +10,18 @@ async def test_gen_stim(dut):
     # Start the clock
     cocotb.fork(clk.start())
 
+    dut.STIM_PERIOD_CYCLES.value = 5000  # 5000 * 2e-6s = 10ms
+    dut.STIM_WIDTH_CYCLES.value = 500  # 1ms pulse width
+    dut.STIM_GAP_CYCLES.value = 500  # 1 ms pulse gap
+
     dut.MEASURE_PRESCALAR.value = 0b0001
-    await Timer(1000, units='ms')
+    await Timer(100, units='ms')
 
     dut.MEASURE_PRESCALAR.value = 0b0011
-    await Timer(1000, units='ms')
+    await Timer(100, units='ms')
 
     dut.MEASURE_PRESCALAR.value = 0b0111
-    await Timer(1000, units='ms')
+    await Timer(100, units='ms')
+
+    dut.STIM_PERIOD_CYCLES.value = 2500  # 2500 * 2e-6s = 5ms
+    await Timer(30, units='ms')
